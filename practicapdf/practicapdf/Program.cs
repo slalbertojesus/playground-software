14ffd8cbc79776b80d0cabf88105aa22ed73b6f9
Alberto Sánchez <slalbertojesus@gmail.com>
2019-08-31 20:33:24 -0700

    Se añadió filas para datos varios

--- practicapdf/practicapdf/Program.cs
+++ practicapdf/practicapdf/Program.cs
@@ -21,7 +21,7 @@ namespace practicapdf
         static void Main()
         {
             FileStream fs = new FileStream
-                ("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+                ("Solicitud.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
             Font font = new Font();
             font.SetColor(255, 255, 255);
             Document doc = new Document();
@@ -76,6 +76,99 @@ namespace practicapdf
             table.AddCell("CURP: ");
             table.AddCell("Genero: ");
             table.AddCell(fechaGeneroCurp);
+
+            PdfPCell rfcFielDomicilioNoExtNoInt = new PdfPCell(new Phrase("No ext: "));
+            rfcFielDomicilioNoExtNoInt.Colspan = 5;
+            table.AddCell("RFC: ");
+            table.AddCell("FIEL: ");
+            table.AddCell("Domicilio: ");
+            table.AddCell("No int: ");
+            table.AddCell(rfcFielDomicilioNoExtNoInt);
+
+            PdfPCell coloniaCpDelegacion = new PdfPCell(new Phrase("Delegacion: "));
+            coloniaCpDelegacion.Colspan = 5;
+            table.AddCell("Colonia: ");
+            table.AddCell("Cp: ");
+            table.AddCell(coloniaCpDelegacion);
+
+            PdfPCell ciudadEstadoPaisTiempoRecidencia = new PdfPCell(new Phrase("Tiempo de recidencia: "));
+            ciudadEstadoPaisTiempoRecidencia.Colspan = 5;
+            table.AddCell("Ciudad: ");
+            table.AddCell("Estado: ");
+            table.AddCell("Pais: ");
+            table.AddCell(ciudadEstadoPaisTiempoRecidencia);
+
+            PdfPCell tipoDomicilio = new PdfPCell(new Phrase("Tipo de domicilio: "));
+            tipoDomicilio.Colspan = 5;
+            table.AddCell(tipoDomicilio);
+
+            PdfPCell telefono = new PdfPCell(new Phrase("Casa: "));
+            telefono.Colspan = 5;
+            table.AddCell("Telefonos Oficina:");
+            table.AddCell("Celular:");
+            table.AddCell(telefono);
+
+            PdfPCell estadoNacionalidad = new PdfPCell(new Phrase("Nacionalidad: "));
+            estadoNacionalidad.Colspan = 5;
+            table.AddCell("Estado civil:");
+            table.AddCell(estadoNacionalidad);
+
+            PdfPCell correoNacimientoEstadoPais = new PdfPCell(new Phrase("Pais: "));
+            correoNacimientoEstadoPais.Colspan = 5;
+            table.AddCell("Correo:");
+            table.AddCell("Lugar de nacimiento:");
+            table.AddCell("Estado:");
+            table.AddCell(correoNacimientoEstadoPais);
+
+            PdfPCell INFORMACION_EMPLEO = new PdfPCell(new Phrase
+                ("INFORMACIÓN DEL EMPLEO", font));
+            INFORMACION_EMPLEO.BackgroundColor = BaseColor.BLACK;
+            INFORMACION_EMPLEO.Colspan = 5;
+            INFORMACION_EMPLEO.HorizontalAlignment = Element.ALIGN_CENTER;
+            table.AddCell(INFORMACION_EMPLEO);
+
+            PdfPCell empresaNoEmpleado = new PdfPCell(new Phrase("Número empleado: "));
+            empresaNoEmpleado.Colspan = 5;
+            table.AddCell("Nombre empresa:");
+            table.AddCell(empresaNoEmpleado);
+
+            PdfPCell centroTrabajoAntiguedad = new PdfPCell(new Phrase("Antiguedad en el empleo: "));
+            empresaNoEmpleado.Colspan = 5;
+            table.AddCell("Centro de trabajo:");
+            table.AddCell(empresaNoEmpleado);
+
+            PdfPCell ocupacionPuestoPresentacion = new PdfPCell(new Phrase("Periodo de presentación: "));
+            ocupacionPuestoPresentacion.Colspan = 5;
+            table.AddCell("Ocupación:");
+            table.AddCell("Puesto:");
+            table.AddCell(ocupacionPuestoPresentacion);
+
+            PdfPCell percepcionesLiquidoPercepcionesLiquido = new PdfPCell(new Phrase("Líquido recibo 2: "));
+            ocupacionPuestoPresentacion.Colspan = 5;
+            table.AddCell("Percepciones recibo 1:");
+            table.AddCell("Líquido recibo 1:");
+            table.AddCell("Percepciones recibo 2:");
+            table.AddCell("Líquido recibo 1:");
+            table.AddCell(percepcionesLiquidoPercepcionesLiquido);
+
+            PdfPCell REFERENCIA_PERSONAL = new PdfPCell(new Phrase
+                ("REFERENCIAS PERSONALES", font));
+            REFERENCIA_PERSONAL.BackgroundColor = BaseColor.BLACK;
+            REFERENCIA_PERSONAL.Colspan = 5;
+            REFERENCIA_PERSONAL.HorizontalAlignment = Element.ALIGN_CENTER;
+            table.AddCell(REFERENCIA_PERSONAL);
+
+            PdfPCell nombreRelacionTelefono = new PdfPCell(new Phrase("Telefono: "));
+            nombreRelacionTelefono.Colspan = 5;
+            table.AddCell("Nombre:");
+            table.AddCell("Relación:");
+            table.AddCell(nombreRelacionTelefono);
+
+            PdfPCell direccionHorario = new PdfPCell(new Phrase("Direcicón: "));
+            direccionHorario.Colspan = 5;
+            table.AddCell("Horario:");
+            table.AddCell(direccionHorario);
+
 
             table.WidthPercentage = 100;
             doc.Add(table);
